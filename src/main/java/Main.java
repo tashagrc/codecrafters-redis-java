4456a3f7384c96f71b245c0158fcc837f0e21b68
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-02-19 09:46:00 +0700

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,14 +9,29 @@ public class Main {
     System.out.println("Logs from your program will appear here!");
 
     //  Uncomment this block to pass the first stage
-       ServerSocket serverSocket = null;
+       ServerSocket serverSocket;
        Socket clientSocket = null;
+       String serverResponse = "+PONG\r\n";
+       String clientMessage;
        int port = 6379;
        try {
          serverSocket = new ServerSocket(port);
          serverSocket.setReuseAddress(true);
          // Wait for connection from client.
          clientSocket = serverSocket.accept();
+
+         // baca tulis input output dr socket
+         BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+         PrintWriter outWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+
+         // untuk setiap line yg disend client, server respon pong
+         while((clientMessage = input.readLine()) != null) {
+          System.out.println("Command received: " + clientMessage);
+          if(clientMessage.equalsIgnoreCase("ping")) {
+            outWriter.print(serverResponse);
+            outWriter.flush();
+          }
+         }
        } catch (IOException e) {
          System.out.println("IOException: " + e.getMessage());
        } finally {
